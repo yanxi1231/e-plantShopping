3a06ea0fbdbb72f03e4a5e734ddf17e1de5fe186
yanxi1231 <na.ya@northeastern.edu>
2025-08-09 01:56:43 -0400

    initial

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
@@ -212,6 +213,8 @@ function ProductList({ onHomeClick }) {
             ]
         }
     ];
+
+
     const styleObj = {
         backgroundColor: '#4CAF50',
         color: '#fff!important',
@@ -233,6 +236,7 @@ function ProductList({ onHomeClick }) {
         textDecoration: 'none',
     }
 
+    
     const handleHomeClick = (e) => {
         e.preventDefault();
         onHomeClick();
@@ -252,9 +256,11 @@ function ProductList({ onHomeClick }) {
         e.preventDefault();
         setShowCart(false);
     };
+
     return (
         <div>
             <div className="navbar" style={styleObj}>
+
                 <div className="tag">
                     <div className="luxury">
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
@@ -265,13 +271,14 @@ function ProductList({ onHomeClick }) {
                             </div>
                         </a>
                     </div>
-
                 </div>
+
                 <div style={styleObjUl}>
                     <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
                     <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
+
             {!showCart ? (
                 <div className="product-grid">
 
