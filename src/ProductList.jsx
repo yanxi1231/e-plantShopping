44e9923b4d1b80f4cef77bdc494d5b6ae59838aa
yanxi1231 <na.ya@northeastern.edu>
2025-08-10 14:39:17 -0400

    edit cart

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -313,12 +313,17 @@ function ProductList({ onHomeClick }) {
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
                                 <rect width="156" height="156" fill="none"></rect>                                                                                                                           
                                 <circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle>
-                                <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                                <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>                               
+                                <text 
+                                    x="128" 
+                                    y="140" 
+                                    textAnchor="middle" 
+                                    className="cart-quantity-count"
+                                    fill='white'                                   
+                                >
+                                    {calculateTotalQuantity()}
+                                </text>
                             </svg>
-                            {/* Badge to show quantity */}
-                            <span className="cart-quantity-count">
-                                {calculateTotalQuantity()}
-                            </span>
                         </h1>
                         </a>
                     </div>
