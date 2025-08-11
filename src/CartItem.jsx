46880dd46ec4946c9b8804d61e8d746c8f381324
yanxi1231 <na.ya@northeastern.edu>
2025-08-11 02:12:52 -0400

    edit cartitem method

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -30,13 +30,13 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleDecrement = (item) => {
-    if (cart[item].quantity > 0) {
-        dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 })); 
+    if (item.quantity > 0) {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 })); 
     }   
   };
 
   const handleRemove = (item) => {
-    dispatch(removeItem(item));
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
@@ -69,7 +69,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e)=> handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
